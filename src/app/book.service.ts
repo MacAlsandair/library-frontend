9d9dfa1e8cd10de4ce272efd96c64d8e12fc9e3c
MacAlsandair <mac.alsandair@gmail.com>
2023-04-26 12:22:30 +0200

    Add get, add, update and delete methods in BookService

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-  private 
+  private apiServerUrl = environment.apiServerUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiServerUrl}/api/book/all`);
+  }
+
+  public addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(`${this.apiServerUrl}/api/book/add`, book)
+  }
+
+  public updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiServerUrl}/api/book/update`, book);
+  }
+
+  public deleteBook(bookId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/book/delete/${bookId}`);
+  }
 }
