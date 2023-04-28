873b01c614a6df8badd3eba101191cf783432105
MacAlsandair <mac.alsandair@gmail.com>
2023-04-28 13:43:33 +0200

    Add book edit modal component

---
+++ src/app/book-edit-modal/book-edit-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book-edit-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book-edit-modal.component.html',
+  styleUrls: ['./book-edit-modal.component.scss']
+})
+export class BookEditModalComponent {
+
+}
