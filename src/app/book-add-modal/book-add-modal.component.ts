7430fabc84f5685c8cdfc3ffbb127723d8134b11
MacAlsandair <mac.alsandair@gmail.com>
2023-04-29 10:33:32 +0200

    Add FormsModule import and ommit input decorator for addedBook in BookAddModalComponent

--- src/app/book-add-modal/book-add-modal.component.ts
+++ src/app/book-add-modal/book-add-modal.component.ts
@@ -1,16 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Book } from '../book-card/book';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-add-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-add-modal.component.html',
   styleUrls: ['./book-add-modal.component.scss']
 })
 export class BookAddModalComponent {
-  @Input() addedBook!: Book;
+  addedBook!: Book;
   @Output() addBookEvent = new EventEmitter<Book>();
 
   constructor () {}
