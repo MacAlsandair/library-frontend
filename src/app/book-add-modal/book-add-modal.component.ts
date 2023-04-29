fdb723abef50f478e434037a310c99ff2996e2e7
MacAlsandair <mac.alsandair@gmail.com>
2023-04-29 10:18:22 +0200

    Generate book-add-modal component

---
+++ src/app/book-add-modal/book-add-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book-add-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book-add-modal.component.html',
+  styleUrls: ['./book-add-modal.component.scss']
+})
+export class BookAddModalComponent {
+
+}
