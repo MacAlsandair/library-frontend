3d26a70aff42b3f1c2b3b1c98479efeac117e79d
MacAlsandair <mac.alsandair@gmail.com>
2023-06-18 17:53:18 +0200

    Add login component at the main page, fix bug with login.component.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,23 @@ import { AppComponent } from './app.component';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 import { BookCardComponent } from './book-card/book-card.component';
-import { LoginComponent } from './login/login.component';
+//import {PersonComponent} from './person/person.component';
+import {LoginComponent} from './login/login.component';
+//import {AuthInterceptor} from './auth/auth.interceptor';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+//import {PersonEditComponent} from './person/edit/person-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +38,19 @@ import { LoginComponent } from './login/login.component';
     FormsModule, 
     HttpClientModule, NgbModule,
     MainNavbarComponent,
-    BookCardComponent
+    BookCardComponent,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    LayoutModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [NgbActiveModal],
   bootstrap: [AppComponent]
