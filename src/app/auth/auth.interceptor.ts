9fb8655352f72f9c45c0260f1c6361ee109decdc
MacAlsandair <mac.alsandair@gmail.com>
2023-06-18 18:08:51 +0200

    Add logic into AuthInterceptor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -3,16 +3,38 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  constructor(private router: Router) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      let token = sessionStorage.getItem("app.token");
+      if (token) {
+          request = request.clone({
+              setHeaders: {
+                  Authorization: `Bearer ${token}`
+              },
+          });
+      }
+
+      return next.handle(request).pipe(
+          catchError((error: HttpErrorResponse) => this.handleErrorRes(error))
+      );
+  }
+
+  private handleErrorRes(error: HttpErrorResponse): Observable<never> {
+      if (error.status === 401) {
+          this.router.navigateByUrl("/login", {replaceUrl: true});
+      }
+      return throwError(() => error);
   }
 }
