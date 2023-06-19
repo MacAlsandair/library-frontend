2eb133013729878ff7cce72afce0abb83aded91f
MacAlsandair <mac.alsandair@gmail.com>
2023-06-19 19:18:58 +0200

    Replace old design of login page with new one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,17 +16,10 @@ export class AppComponent {
 
   constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) {
   }
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-          map(result => result.matches),
-          shareReplay()
-      );
-
   logout() {
       this.authService.logout();
 
-      this.router.navigateByUrl("/");
+      //this.router.navigateByUrl("/");
   }
 
 }
