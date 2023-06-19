0b08c6358e1844ebf143f8b85634d9b557a70fb2
MacAlsandair <mac.alsandair@gmail.com>
2023-06-19 17:10:17 +0200

    Add interception of any requests to the server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { BookCardComponent } from './book-card/book-card.component';
+import { BookEditModalComponent } from './book-edit-modal/book-edit-modal.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'books', component: AppComponent, canActivate: [AuthGuard], data: {role: 'ROLE_USER'}},
+  {path: 'book/:id', component: BookEditModalComponent, canActivate: [AuthGuard], data: {role: 'ROLE_USER'}},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: 'book-card'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, NgModule } from '@angular/core';
+import { Observable, map, shareReplay } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,5 +12,21 @@ import { Component, NgModule } from '@angular/core';
 })
 export class AppComponent {
   title = 'library-frontend';
+
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) {
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+          map(result => result.matches),
+          shareReplay()
+      );
+
+  logout() {
+      this.authService.logout();
+
+      this.router.navigateByUrl("/");
+  }
 
 }
