4ebba21fe6972ab663ae81b50e6b9fb9221922b6
MacAlsandair <mac.alsandair@gmail.com>
2023-06-19 18:21:18 +0200

    Change extension of login.component to scss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import jwtDecode, {JwtPayload} from 'jwt-decode';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
