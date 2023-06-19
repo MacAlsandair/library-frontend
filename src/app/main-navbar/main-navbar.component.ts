df2d5290e430e1b3a6e60d939692e6c242094c52
MacAlsandair <mac.alsandair@gmail.com>
2023-06-19 21:05:28 +0200

    Add logic for login, logout and sign up elements of navbar

--- src/app/main-navbar/main-navbar.component.ts
+++ src/app/main-navbar/main-navbar.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-navbar',
   templateUrl: './main-navbar.component.html',
+  imports: [CommonModule],
   standalone: true,
   styleUrls: ['./main-navbar.component.scss']
 })
 export class MainNavbarComponent {
 	active = 1;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(public authService: AuthService, private router: Router) {
   }
 
   logout() {
