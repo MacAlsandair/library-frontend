e2b0e19ed4b9bd54d61639630d4dd1cf003b52e2
MacAlsandair <mac.alsandair@gmail.com>
2023-06-29 18:24:53 +0200

    Create a logic of retrieving recommended books

--- src/app/recommended-books/recommended-books.component.ts
+++ src/app/recommended-books/recommended-books.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Book } from '../book-card/book';
+import { BookService } from '../book-card/book.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-recommended-books',
@@ -6,7 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./recommended-books.component.scss']
 })
 export class RecommendedBooksComponent {
+  public books!: Book[];
 
-  
+  constructor (public bookService: BookService, private modalService: NgbModal) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  public getBooks(): void {
+    this.bookService.getPersonalRecommendations().subscribe(
+      (response: Book[]) => {
+        this.books = response
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
 }
