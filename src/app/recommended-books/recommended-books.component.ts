927a605e8737554799c2d59ddb1a6956c0fbfdac
MacAlsandair <mac.alsandair@gmail.com>
2023-06-29 18:22:33 +0200

    Create an html template for book recommendations

---
+++ src/app/recommended-books/recommended-books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-books',
+  templateUrl: './recommended-books.component.html',
+  styleUrls: ['./recommended-books.component.scss']
+})
+export class RecommendedBooksComponent {
+
+  
+
+}
