0c8a66cb72e3db17bbb107fd97813e04691770c0
MacAlsandair <mac.alsandair@gmail.com>
2023-07-01 17:01:39 +0200

    Add fetching book from bookId from URL, and then fetch it from server via BookService

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,25 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Book } from '../book-card/book';
 import { BookService } from '../book-card/book.service';
+import { switchMap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit, OnDestroy {
   @Input() book!: Book;
+  private unsubscribe$ = new Subject<void>();
 
-  constructor(private bookService: BookService) { }
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getBook();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   addToFavorites(): void {
-    this.bookService.addBookToFavorites(this.book.id).subscribe();
+    this.bookService.addBookToFavorites(this.book.id).pipe(takeUntil(this.unsubscribe$)).subscribe();
   }
 
   removeFromFavorites(): void {
-    this.bookService.deleteBookFromFavorites(this.book.id).subscribe();
+    this.bookService.deleteBookFromFavorites(this.book.id).pipe(takeUntil(this.unsubscribe$)).subscribe();
   }
+
+  private getBook(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.bookService.getBookById(+id).pipe(takeUntil(this.unsubscribe$)).subscribe(book => this.book = book);
+    } else {
+      console.error('Book id is not available');
+    }
+  }
+  
 }
